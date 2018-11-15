da97f21369e4baf8a36e603891ea99d093279491
Nicholas Koh <43373275+nc-khs@users.noreply.github.com>
2018-11-16 03:00:44 +0800

    Added package info for javadocs

---
+++ src/scrame/print/package-info.java
@@ -0,0 +1,9 @@
+/**
+ *
+ * Package to include files that support data output to the user through console printing.
+ *
+ * @author  CZ2002 SS3 Group 1
+ * @version 5.0
+ */
+
+package scrame.print;
