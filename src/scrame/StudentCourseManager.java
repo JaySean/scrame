a43ea4f8f9a84172af17d827c6fcc48fb605371f
puppy0022 <32283437+lostperson0022@users.noreply.github.com>
2018-11-16 12:06:32 +0800

    Did up javadocs for StudentCourseManager

--- src/scrame/StudentCourseManager.java
+++ src/scrame/StudentCourseManager.java
@@ -2,10 +2,24 @@ package scrame;
 
 import java.util.ArrayList;
 
-public class StudentCourseManager {
+/**
+ * Control class that manages the marks of a student in all his components
+ * @author CZ2002 SS3 Group 1
+ * @version 2.0
+ */
 
+public class StudentCourseManager {
+    /**
+     * Storing data on a student's performance in a particular course in a .txt file
+     */
     final static String studentCourseFile = "data/StudentCourse.txt";
 
+    /**
+     * Gets the index when raversing through the Student records
+     * @param studentMatric The matric number of the student
+     * @param courseCode The code unique to the course.
+     * @return the index of the record being searched
+     */
     public static int getIndex(String studentMatric, String courseCode) {
         for (int i = 0; i < Main.studentCourseList.size(); i++) {
             if (Main.studentCourseList.get(i).getStudentMatric().equals(studentMatric)) {
@@ -17,6 +31,12 @@ public class StudentCourseManager {
         return -1;
     }
 
+    /**
+     * getter method to get the record of a student
+     * @param studentMatric matric number of the student
+     * @param courseCode course code of the student
+     * @return the record of the student
+     */
     public static StudentCourse getStudentCourse(String studentMatric, String courseCode) {
         StudentCourse studentCourse;
 
@@ -31,6 +51,10 @@ public class StudentCourseManager {
         return studentCourse;
     }
 
+    /**
+     * updates student oourse record
+     * @param studentCourse the record of a student in a particular course
+     */
     public static void updateStudentCourse(StudentCourse studentCourse) {
         if (studentCourse == null) return;
         int index = getIndex(studentCourse.getStudentMatric(), studentCourse.getCourseCode());
@@ -42,6 +66,11 @@ public class StudentCourseManager {
         DatabaseManager.write(Main.studentCourseList, studentCourseFile);
     }
 
+    /**
+     * instantiates an array list of course names along with its course code
+     * @param courseCode the unique code of a course
+     * @return list of courses in the array list
+     */
     public static ArrayList<StudentCourse> getCourse(String courseCode) {
         ArrayList<StudentCourse> listByCourse = new ArrayList<>(0);
         for (StudentCourse studentCourse : Main.studentCourseList) {
@@ -52,6 +81,11 @@ public class StudentCourseManager {
         return listByCourse;
     }
 
+    /**
+     * calculates the eaverage exam marks of a course
+     * @param courseCode unique code of courses
+     * @return average marks students got for the exam of a course
+     */
     public static double getAveExamMarks(String courseCode) {
         ArrayList<StudentCourse> studentCourseList = getCourse(courseCode);
         int totalMarks = 0;
@@ -61,6 +95,11 @@ public class StudentCourseManager {
         return (double) totalMarks / studentCourseList.size();
     }
 
+    /**
+     * Calculates average marks students got for the assignment component of the course
+     * @param courseCode Unique code to a course
+     * @return average marks students got for the assignment component
+     */
     public static double getAveAssignmentMarks(String courseCode) {
         ArrayList<StudentCourse> studentCourseList = getCourse(courseCode);
         int totalMarks = 0;
@@ -69,7 +108,11 @@ public class StudentCourseManager {
         }
         return (double) totalMarks / studentCourseList.size();
     }
-
+    /**
+     * Calculates average marks students got for the class part component of the course
+     * @param courseCode Unique code to a course
+     * @return average marks students got for the class part component
+     */
     public static double getAveClassPartMarks(String courseCode) {
         ArrayList<StudentCourse> studentCourseList = getCourse(courseCode);
         int totalMarks = 0;
